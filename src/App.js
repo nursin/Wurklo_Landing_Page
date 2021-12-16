8bcf3ba63b96421efd896524b69e01730abff813
Bobby Keel <bobbykeel76@gmail.com>
2021-12-16 11:00:20 -0600

    background logo, name, token, displayed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='app__main'>
+        {/* <h1>Wurklo</h1>
+        <p>We're going to put the power of a large business in the palm of your hand, so you can work how you want! We want the same for those who purchase your services!</p>
+        <p>Wurklo focuses on accelerating and improving the service industry by providing workers with advanced software in a simple design that will give the average person the power of a large scale business in the palm of their hands. Wurklo strives to provide an equal level of power to people who want to purchase those services, and leverages blockchain power to integrate payments on a global scale.</p> */}
+      </div>
     </div>
   );
 }
