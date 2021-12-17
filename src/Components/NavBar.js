8e9eef6c505b4f6dc121646a3614f0560fb6a56a
Bobby Keel <bobbykeel76@gmail.com>
2021-12-17 11:56:38 -0600

    more styling

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -17,14 +17,14 @@ function NavBar() {
     return (
         <div className='container-fluid g-0 g-sm-4'>
             <div className="row g-0">
-                <div className="col">
-                    <Navbar sticky className='' color="clear" dark expand="xl">
+                <div className="col d-sm-flex justify-content-sm-center">
+                    <Navbar className='sticky-top' color="clear" dark expand="xl">
                         <NavbarBrand className='fs-1' href="/">
                             <img className='logo-image' src={logo} alt="Wurklo" />
                         </NavbarBrand>
                         <NavbarToggler className='text-white' onClick={() => setToggleNav(!toggleNav)} onBlur={() => setToggleNav(false)} />
                         <Collapse isOpen={toggleNav} navbar>
-                            <Nav className="ms-2 w-50" navbar>
+                            <Nav className="ms-2" navbar>
                                 <NavItem className='px-sm-2'>
                                     <NavLink className='p-sm-2' href="/">Tokenomics</NavLink>
                                 </NavItem>
