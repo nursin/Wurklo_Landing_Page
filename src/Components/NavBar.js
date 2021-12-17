63bffda38e0eb318ae427e754a6b1284cdce5782
Bobby Keel <bobbykeel76@gmail.com>
2021-12-17 11:46:01 -0600

    logo with alignment

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -15,7 +15,7 @@ function NavBar() {
     const [toggleNav, setToggleNav] = useState(false);
 
     return (
-        <div className='container-fluid g-1 g-sm-4'>
+        <div className='container-fluid g-0 g-sm-4'>
             <div className="row g-0">
                 <div className="col">
                     <Navbar sticky className='' color="clear" dark expand="xl">
@@ -24,7 +24,7 @@ function NavBar() {
                         </NavbarBrand>
                         <NavbarToggler className='text-white' onClick={() => setToggleNav(!toggleNav)} onBlur={() => setToggleNav(false)} />
                         <Collapse isOpen={toggleNav} navbar>
-                            <Nav className="ml-0" navbar>
+                            <Nav className="ms-2 w-50" navbar>
                                 <NavItem className='px-sm-2'>
                                     <NavLink className='p-sm-2' href="/">Tokenomics</NavLink>
                                 </NavItem>
