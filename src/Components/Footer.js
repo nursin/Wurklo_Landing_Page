d47e30db9ef2168986a4ff4b7523cb6f0ddcfeb7
Bobby Keel <bobbykeel76@gmail.com>
2021-12-17 09:16:24 -0600

    social icons on mobile to close to edge fix, changes from px-2 to px-1

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,14 +9,14 @@ function Footer() {
         <div className='container'>
             <div className="row">
                 <div className="col col-sm-7 mx-auto mb-5 text-center">
-                    <a className="social-icons px-2 px-sm-4" href="https://t.me/+-tLGe4BSh1pkODYx"><FontAwesomeIcon className='text-white fs-1 telegram' icon={faTelegram}></FontAwesomeIcon></a>{' '}
-                    <a className="social-icons px-2 px-sm-4" href="https://www.facebook.com/Wurklo"><FontAwesomeIcon className='text-white fs-1 facebook' icon={faFacebook}></FontAwesomeIcon></a>{' '}
-                    <a className="social-icons px-2 px-sm-4" href="https://twitter.com/Wurklo"><FontAwesomeIcon className='text-white fs-1 twitter' icon={faTwitter}></FontAwesomeIcon></a>{' '}
-                    <a className="social-icons px-2 px-sm-4" href="https://www.reddit.com/user/Wurklo/comments/ri14n4/what_is_wurklo/"><FontAwesomeIcon className='text-white fs-1 reddit' icon={faReddit}></FontAwesomeIcon></a>{' '}
-                    <a className="social-icons px-2 px-sm-4" href=" https://www.instagram.com/wurklo/"><FontAwesomeIcon className='text-white fs-1 instagram' icon={faInstagram}></FontAwesomeIcon></a>{' '}
-                    <a className="social-icons px-2 px-sm-4" href="https://discord.gg/7sK89kZf"><FontAwesomeIcon className='text-white fs-1 discord' icon={faDiscord}></FontAwesomeIcon></a>{' '}
-                    <a className="social-icons px-2 px-sm-4" href="https://www.youtube.com/channel/UCxGURLHBrraodONmnB7jxcw"><FontAwesomeIcon className='text-white fs-1 youtube' icon={faYoutube}></FontAwesomeIcon></a>{' '}
-                    <a className="social-icons px-2 px-sm-4" href="https://www.tiktok.com/@wurklo"><FontAwesomeIcon className='text-white fs-1 tiktok' icon={faTiktok}></FontAwesomeIcon></a>{' '}
+                    <a className="social-icons px-1 px-sm-4" href="https://t.me/+-tLGe4BSh1pkODYx"><FontAwesomeIcon className='text-white fs-1 telegram' icon={faTelegram}></FontAwesomeIcon></a>{' '}
+                    <a className="social-icons px-1 px-sm-4" href="https://www.facebook.com/Wurklo"><FontAwesomeIcon className='text-white fs-1 facebook' icon={faFacebook}></FontAwesomeIcon></a>{' '}
+                    <a className="social-icons px-1 px-sm-4" href="https://twitter.com/Wurklo"><FontAwesomeIcon className='text-white fs-1 twitter' icon={faTwitter}></FontAwesomeIcon></a>{' '}
+                    <a className="social-icons px-1 px-sm-4" href="https://www.reddit.com/user/Wurklo/comments/ri14n4/what_is_wurklo/"><FontAwesomeIcon className='text-white fs-1 reddit' icon={faReddit}></FontAwesomeIcon></a>{' '}
+                    <a className="social-icons px-1 px-sm-4" href=" https://www.instagram.com/wurklo/"><FontAwesomeIcon className='text-white fs-1 instagram' icon={faInstagram}></FontAwesomeIcon></a>{' '}
+                    <a className="social-icons px-1 px-sm-4" href="https://discord.gg/7sK89kZf"><FontAwesomeIcon className='text-white fs-1 discord' icon={faDiscord}></FontAwesomeIcon></a>{' '}
+                    <a className="social-icons px-1 px-sm-4" href="https://www.youtube.com/channel/UCxGURLHBrraodONmnB7jxcw"><FontAwesomeIcon className='text-white fs-1 youtube' icon={faYoutube}></FontAwesomeIcon></a>{' '}
+                    <a className="social-icons px-1 px-sm-4" href="https://www.tiktok.com/@wurklo"><FontAwesomeIcon className='text-white fs-1 tiktok' icon={faTiktok}></FontAwesomeIcon></a>{' '}
                 </div>
             </div>
         </div>
